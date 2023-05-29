4dfbc175dcad324b19d8c64e6fdd68228d3d4401
2101032 <2101032@s.asojuku.ac.jp>
2023-05-29 10:45:16 +0900

    飯干

---
+++ コード/resettingPassword.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+    <html lang="ja">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- cssの導入 -->
+      <link rel="stylesheet" href="./css/resettingMPassword.css">
+      <link rel="stylesheet" href="./css/style.css?v=2">
+          <!-- javascriptの導入 -->
+    <script src="./script/script.js"></script>
+    
+    <!-- bootstrapのCSSの導入 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+      <title>パスワードの再設定画面</title>
+
+
+    </head>
+
+    <body>
+    <form action="login.php" method="post">
+  <div class="container">
+<div class="child" style="text-align:center">
+  <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto">
+    <div class="fontMeiryoBold">
+    <p>パスワードの再設定</p>
+    <p class="mb-2">・前のパスワード</p>
+    <p><input type="password" name="beforepassword" value="" class="form-control mb-2" placeholder="既存パスワード"></p>
+    <p class="mb-2">・新しいパスワード</p>
+    <p><input type="password" name="afterpassword" value="" class="form-control mb-2" placeholder="新規パスワード"></p>
+    <p><input type="submit" value="変更する" class="btn btn-warning"></p>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+    </body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    </html>
+    
+    <!-- cssの導入 -->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <!-- <link rel="stylesheet" href="css/resettingMailaddress.css"> -->
+
+    <!-- javascriptの導入 -->
+    <!-- <script src="./script/script.js"></script> -->
+    
+    <!-- bootstrapのCSSの導入 -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    
+<!-- <form action="home.php" method="post">
+  <div class="container">
+<div class="child" style="text-align:center"> -->
+  <!-- <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto"> -->
+    <!-- <p>メールアドレスの再設定</p>
+    <p class="mb-2">・前のメールアドレス</p>
+    <p><input type="text" name="beforeMail" value=""></p>
+    <p class="mb-2">・新しいメールアドレス</p>
+    <p><input type="text" name="afterMail" value=""></p>
+    <p><input type="submit" value="変更する" class="btn btn-warning"></p> -->
+    <!-- </div> -->
+  <!-- </div>
+</div>
+</form> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script> -->
+
+
+    <!-- <div class="fontMeiryoBold" style="text-align:center">
+       <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto"> 
+        <p>メールアドレスの再設定</p>
+         <p class="mb-2">・前のメールアドレス</p>
+          <input type="text" name="beforeMail" value="" class="form-control mb-2"> 
+          <p class="mb-2">・新しいメールアドレス</p> 
+          <input type="text" name="afterMail" value="" class="form-control mb-2"> 
+          <input type="submit" value="変更する" class="btn btn-warning">
+         </div>
+         </div> -->
