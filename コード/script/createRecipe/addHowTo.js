ac5332cf0850885210f3173f0776f51f74951e2a
kajimotokajimoto <116693318+TaniTaniTSRTYM@users.noreply.github.com>
2023-06-01 12:25:47 +0900

    Create addHowTo.js

---
+++ コード/script/createRecipe/addHowTo.js
@@ -0,0 +1,44 @@
+// 複数の画像読み込み用
+const fileInput = document.getElementById('recipe_image');
+const handleFileSelect = () => {
+    const files = fileInput.files;
+        for (let i = 0; i < files.length; i++) {
+            previewFile(files[i]);
+        }
+}
+
+// // 画像一つ用
+// const fileInput = document.getElementById('recipe_image');
+// const handleFileSelect = () => {
+//     const files = fileInput.files;
+//         for (let i = 0; i < files.length; i++) {
+//             previewFile(files[i]);
+//         }
+// }
+
+
+function previewFile(file) {
+    // プレビュー画像を追加する要素
+    const preview = document.getElementById('preview');
+
+    // FileReaderオブジェクトを作成
+    const reader = new FileReader();
+
+    // ファイルが読み込まれたときに実行する
+    reader.onload = function (e) {
+        const imageUrl = e.target.result; // 画像のURLはevent.target.resultで呼び出せる
+        const img = document.createElement("img"); // img要素を作成
+        img.src = imageUrl; // 画像のURLをimg要素にセット
+        img.className="previewImg";
+        // preview内の画像をいったん削除して表示されるのを一つにする
+        preview.innerHTML="";
+
+        preview.appendChild(img); // #previewの中に追加
+    }
+
+    // いざファイルを読み込む
+    reader.readAsDataURL(file);
+}
+
+// これ何？(無いと画像が表示されない)
+fileInput.addEventListener('change', handleFileSelect);
