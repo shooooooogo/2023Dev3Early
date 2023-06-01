f71b89217e8969f86d2053cf07119c5bb2430bac
yamashita2101023 <2101023@s.asojuku.ac.jp>
2023-06-01 17:15:50 +0900

    メールアドレス、パスワードの再設定
    メアド、パスワードの再設定の機能追加にあたり
    DAO.php、resetInsertMail.phpに処理を追加
    resettingMailadderss.phpのformタグのファイル名を変更

--- コード/resettingMailaddress.php
+++ コード/resettingMailaddress.php
@@ -19,7 +19,7 @@
     <body>
     <img class="resettingMailaddresslogo" src="img/SumaDeliIcon.png" alt="ロゴです">
 
-    <form action="login.php" method="post">
+    <form action="resetInsertMail.php" method="post">
       <div class="container">
       <div class="child" style="text-align:center">
         <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto">
@@ -38,43 +38,3 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     </html>
-    
-    <!-- cssの導入 -->
-    <!-- <link rel="stylesheet" href="css/style.css"> -->
-    <!-- <link rel="stylesheet" href="css/resettingMailaddress.css"> -->
-
-    <!-- javascriptの導入 -->
-    <!-- <script src="./script/script.js"></script> -->
-    
-    <!-- bootstrapのCSSの導入 -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
-    
-<!-- <form action="home.php" method="post">
-  <div class="container">
-<div class="child" style="text-align:center"> -->
-  <!-- <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto"> -->
-    <!-- <p>メールアドレスの再設定</p>
-    <p class="mb-2">・前のメールアドレス</p>
-    <p><input type="text" name="beforeMail" value=""></p>
-    <p class="mb-2">・新しいメールアドレス</p>
-    <p><input type="text" name="afterMail" value=""></p>
-    <p><input type="submit" value="変更する" class="btn btn-warning"></p> -->
-    <!-- </div> -->
-  <!-- </div>
-</div>
-</form> -->
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script> -->
-
-
-    <!-- <div class="fontMeiryoBold" style="text-align:center">
-       <div class="container d-flex flex-column justify-content-center align-items-center mt-auto mb-auto"> 
-        <p>メールアドレスの再設定</p>
-         <p class="mb-2">・前のメールアドレス</p>
-          <input type="text" name="beforeMail" value="" class="form-control mb-2"> 
-          <p class="mb-2">・新しいメールアドレス</p> 
-          <input type="text" name="afterMail" value="" class="form-control mb-2"> 
-          <input type="submit" value="変更する" class="btn btn-warning">
-         </div>
-         </div> -->
