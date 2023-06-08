97ffb581a66e109c20c5ed7e83d651a72cadbd14
2101005 <116612779+2101005@users.noreply.github.com>
2023-06-08 09:46:09 +0900

    hsa

--- コード/dishDetail.php
+++ コード/dishDetail.php
@@ -90,7 +90,7 @@
     <div class="container-fluid elements">
         
         <!-- 料理のサムネ＆タイトル -->
-        <div class="row pt-2" style="width:100%">
+        <div class="row pt-2">
             <img class="dishImg col-12 offset-1 img-fluid" src="img/PepperRice.png" alt="">
         </div>
         <h3 style="text-align: center;">ぶちうまペッパーライス</h3>
@@ -112,24 +112,25 @@
         </div>
 
         <!-- 都道府県ラベル 表示し河川からdb接続して持ってきて-->
-        <h6>都道府県ラベル： 山口県</h6><br>
+        <br>
+        <h6 style="margin-left:10%">都道府県ラベル： 山口県</h6><br>
 
         <!-- 食費 -->
-        <h3>食費</h3>
+        <h3 style="margin-left:5%">食費</h3>
         <div
             style="color:#FFE300;text-align: center;font-size:40px;"> 505円/1人
         </div>
 
 
         <!-- 材料 -->
-        <h3>材料</h3>
+        <h3 style="margin-left:5%">材料</h3>
 
             <div class="row pt-3" style="">
                 
                 
                 <div class="offset-1 col-4" style="font-weight:bold">材料名</div>
                 <div class="col-3" style="font-weight:bold">分量</div></b>
-                <div class="col-3" style="font-weight:bold">値段(目安)</div>
+                <div class="col-4" style="font-weight:bold">値段(目安)</div>
                 
 
                 <!-- ループ開始位置 DBに接続して材料たちを詳細番号毎に出力 -->
@@ -139,11 +140,22 @@
                     <!--　分量 -->
                     <div class="col-3">100g</div>
                     <!-- 値段 -->
-                    <div class="col-3">100円</div>
+                    <div class="col-3"><div style="text-align:right;">100円</div></div>
 
                 <!-- ループ終了位置 -->
 
-                    
+                    <!-- 材料名 -->
+                    <div class="offset-1 col-4">玉ねぎ</div>
+                    <!--　分量 -->
+                    <div class="col-3">1/4個</div>
+                    <!-- 値段 -->
+                    <div class="col-3"><div style="text-align:right;">25円</div></div>
+                    <!-- 材料名 -->
+                    <div class="offset-1 col-4">ピーマン</div>
+                    <!--　分量 -->
+                    <div class="col-3">1個</div>
+                    <!-- 値段 -->
+                    <div class="col-3"><div style="text-align:right;">40円</div></div>
 
                     
 
